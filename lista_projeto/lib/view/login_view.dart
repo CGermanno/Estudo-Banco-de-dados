3fce6b73bae469327cc17f770afca5e3b3072a19
CGermano <carlosg.gasparello@gmail.com>
2024-03-26 23:21:22 -0300

    att

--- lista_projeto/lib/view/login_view.dart
+++ lista_projeto/lib/view/login_view.dart
@@ -1,17 +1,50 @@
 // ignore_for_file: prefer_const_constructors
-
+ 
 import 'package:flutter/material.dart';
-
+ 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
-
+ 
   @override
   State<LoginView> createState() => _LoginViewState();
 }
-
+/// tetse
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        centerTitle: true,
+      ),
+ 
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+ 
+            OutlinedButton(
+              onPressed: () {
+                //
+                // botao logar
+                //
+              },
+              child: Text('Logar'),
+ 
+            ),
+            OutlinedButton(
+              onPressed: () {
+                //
+                // botao voltar
+                //
+              },
+              child: Text('Voltar'),
+ 
+ 
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
