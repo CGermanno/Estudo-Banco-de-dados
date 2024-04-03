e8d8c1429c06db59b52f3c40b9024bea3c0460a4
CGermanno <carlosg.gasparello@gmail.com>
2024-04-03 12:32:12 -0300

    att

--- lista_projeto/lib/view/login_view.dart
+++ lista_projeto/lib/view/login_view.dart
@@ -1,13 +1,14 @@
 // ignore_for_file: prefer_const_constructors
- 
+
 import 'package:flutter/material.dart';
- 
+
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
- 
+
   @override
   State<LoginView> createState() => _LoginViewState();
 }
+
 /// tetse
 class _LoginViewState extends State<LoginView> {
   @override
@@ -16,40 +17,40 @@ class _LoginViewState extends State<LoginView> {
       appBar: AppBar(
         title: Text('Login'),
         centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 158, 211, 254),
       ),
- 
       body: Center(
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [           
-            // Adicionar a imagem
-            Image.asset(
-              'lib/imagens/a1.jpg',
-              width: 200,
-              height: 200,
-            ),
-         SizedBox(height: 20),
-            
-         Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
- 
-            OutlinedButton(
-              onPressed: () {
-                //
-                // botao logar
-                //
-              },
-              child: Text('Logar'),
- 
+            Container(
+              margin: ,
+              // Adicionar a imagem
+              child: Image.asset(
+                'lib/imagens/a1.jpg',
+                width: 200,
+                height: 200,
+              ),
             ),
-            OutlinedButton(
-              onPressed: () {
-                //
-                // botao voltar
-                //
-              },
-              child: Text('Voltar'),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () {
+                    //
+                    // botao logar
+                    //
+                  },
+                  child: Text('Logar'),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    //
+                    // botao voltar
+                    //
+                  },
+                  child: Text('Voltar'),
                 ),
               ],
             ),
